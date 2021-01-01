61220fe742b3f6efa75d8ea50a17c6ae1291ab27
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-01 21:28:21 +0330

    "printed output became arranged"

--- news/views.py
+++ news/views.py
@@ -2,19 +2,22 @@ from django.http import JsonResponse
 from django.shortcuts import render
 import feedparser
 
-RSS_FEEDS = {' espn': 'https://www.espn.com/espn/rss/news'}
+RSS_FEEDS = {'cnn': 'https://www.espn.com/espn/rss/news'}
 
 
-def get_news(request, publication="espn"):
+def get_news(request, publication="cnn"):
     if request.method != 'GET':
         return JsonResponse({"message": "Method was not GET"}, status=400)
         pass
     else:
         feed = feedparser.parse(RSS_FEEDS[publication])
-        articles_espn = feed['entries']
+        articles_cnn = feed['entries']
 
-        for article in articles_espn:
-            print(article)
-            print("\n")
+        for article in articles_cnn:
+            print("title: \t", article.title_detail.value, "\n")
+            print("summary: \t", article.summary_detail.value, "\n")
+            print("published date: \t", article.published, "\n")
+            print("full news link: \t", article.link, "\n")
+            print("******************", "\n")
 
     return JsonResponse({"message:": "printed in terminal"}, status=200)
