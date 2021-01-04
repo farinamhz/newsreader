f6a3a989dae0d11398d3f1dd804cf7830ed891fe
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-05 01:01:18 +0330

    "filter news by time and number added"

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('read/fill/', views.fill_db, name='fill-db'),
     path('read/items/', views.get_items, name='get-items'),
     path('read/channel/', views.get_channel, name='get-channel'),
+    path('read/items/by-time/', views.get_items_by_time, name='get-items-by-time'),
 ]
