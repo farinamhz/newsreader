b884662eab5c50b1995fac112e0fe6f8b414861d
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-05 01:01:34 +0330

    "filter news by time and number added"

--- news/views.py
+++ news/views.py
@@ -17,13 +17,13 @@ def fill_db(request, publication="cnn"):
 
     # adding news Item
     article_base_channel = feed.feed
-    ch_subtitle_detail = article_base_channel.subtitle_detail.value,
-    ch_title = article_base_channel.title_detail.value,
-    ch_base_link = article_base_channel.title_detail.base,
-    ch_image = article_base_channel.image.href,
-    ch_language = article_base_channel.language,
-    ch_rights_detail = article_base_channel.rights_detail.value,
-    ch_generator = article_base_channel.generator,
+    ch_subtitle_detail = article_base_channel.subtitle_detail.value
+    ch_title = article_base_channel.title_detail.value
+    ch_base_link = article_base_channel.title_detail.base
+    ch_image = article_base_channel.image.href
+    ch_language = article_base_channel.language
+    ch_rights_detail = article_base_channel.rights_detail.value
+    ch_generator = article_base_channel.generator
     ch_updated = article_base_channel.updated
 
     channel = Channel.objects.create(subtitle_detail=ch_subtitle_detail, base_link=ch_base_link,
@@ -58,6 +58,7 @@ def fill_db(request, publication="cnn"):
     return JsonResponse({"id_list": id_list}, status=201)
 
 
+# get all news with filter on numbers
 @api_view(['GET', ])
 def get_items(request):
     items = Item.objects.all()
@@ -70,10 +71,27 @@ def get_items(request):
     return paginator.get_paginated_response(item_ser.data)
 
 
+# get channel
 @api_view(['GET', ])
 def get_channel(request):
     chan = Channel.objects.all()
     paginator = PageNumberPagination()
+    paginator.page_size = 1
     result_page = paginator.paginate_queryset(chan, request)
     chan_ser = ChannelReadSerializer(result_page, many=True)
     return paginator.get_paginated_response(chan_ser.data)
+
+
+@api_view(['GET', ])
+def get_items_by_time(request):
+    # items = Item.objects.all()
+    from_date = datetime.strptime(request.data['from_date'], '%Y-%m-%d %H:%M:%S')
+    to_date = datetime.strptime(request.data['to_date'], '%Y-%m-%d %H:%M:%S')
+    items = Item.objects.filter(published_date__range=(from_date, to_date))
+    ordered = sorted(items, key=lambda item: datetime.strptime(item.published_date, '%Y-%m-%d %H:%M:%S'),
+                      reverse=True)
+    paginator = PageNumberPagination()
+    paginator.page_size = request.data['size']
+    result_page = paginator.paginate_queryset(ordered, request)
+    item_ser = ItemReadSerializer(result_page, many=True)
+    return paginator.get_paginated_response(item_ser.data)
