84e51f3f49652d1380a05f9d274ae2bbb8833773
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-05 00:16:49 +0330

    "views updated for fill db, getting items,and channel"

--- news/views.py
+++ news/views.py
@@ -3,56 +3,77 @@ import email.utils as x
 from datetime import datetime
 import feedparser
 from rest_framework.decorators import api_view
+from .models import Item, Channel
 from rest_framework.pagination import PageNumberPagination
-
-RSS_FEEDS = {'cnn': 'http://rss.cnn.com/rss/edition.rss'}
+from .serializers import ItemReadSerializer, ChannelReadSerializer
 
 
 @api_view(['GET', ])
-def get_items(request, publication="cnn"):
+def fill_db(request, publication="cnn"):
+    RSS_FEEDS = {'cnn': 'http://rss.cnn.com/rss/edition.rss'}
     feed = feedparser.parse(RSS_FEEDS[publication])
     articles_cnn = feed['entries']
+    id_list = []
 
-    output_list = []
+    # adding news Item
+    article_base_channel = feed.feed
+    ch_subtitle_detail = article_base_channel.subtitle_detail.value,
+    ch_title = article_base_channel.title_detail.value,
+    ch_base_link = article_base_channel.title_detail.base,
+    ch_image = article_base_channel.image.href,
+    ch_language = article_base_channel.language,
+    ch_rights_detail = article_base_channel.rights_detail.value,
+    ch_generator = article_base_channel.generator,
+    ch_updated = article_base_channel.updated
 
+    channel = Channel.objects.create(subtitle_detail=ch_subtitle_detail, base_link=ch_base_link,
+                                     title=ch_title, image=ch_image, language=ch_language,
+                                     rights_detail=ch_rights_detail, generator=ch_generator,
+                                     updated=ch_updated)
+    channel.save()
+    id_list.append(channel.id)
+
+    # adding news Item
     for article in articles_cnn:
-        context = {}
+        published_date = title = summary = link = image_url = ''
         if hasattr(article, 'published'):
-            # print(datetime.fromtimestamp(x.mktime_tz(x.parsedate_tz(article.published))))
-            context["published_date"] = str(datetime.fromtimestamp(x.mktime_tz(x.parsedate_tz(article.published))))
+            published_date = str(datetime.fromtimestamp(x.mktime_tz(x.parsedate_tz(article.published))))
         else:
             continue
         if hasattr(article, 'title_detail'):
-            context["title"] = article.title_detail.value
+            title = article.title_detail.value
         if hasattr(article, 'summary_detail'):
-            context["summary"] = article.summary_detail.value
+            summary = article.summary_detail.value
         if hasattr(article, 'link'):
-            context["link"] = article.link
+            link = article.link
         if hasattr(article, 'media_content'):
-            context["image_url"] = article.media_content[0]["url"]
-        output_list.append(context)
-
-    output_list.sort(key=lambda item: datetime.strptime(item['published_date'], '%Y-%m-%d %H:%M:%S'), reverse=True)
-    # for a in output_list:
-    #     print(a, "\n")
-
-    return JsonResponse(output_list, status=200, safe=False)
+            image_url = article.media_content[0]["url"]
+        try:
+            item = Item.objects.create(published_date=published_date, title=title, summary=summary,
+                                       link=link, image_url=image_url)
+            item.save()
+            id_list.append(item.id)
+        except Exception as e:
+            return JsonResponse({"message": str(e)}, status=400)
+    return JsonResponse({"id_list": id_list}, status=201)
 
 
 @api_view(['GET', ])
-def get_channel(request, publication="cnn"):
-    feed = feedparser.parse(RSS_FEEDS[publication])
-    article_base_channel = feed.feed
-    article_channel_context = {"subtitle_detail": article_base_channel.subtitle_detail.value,
-                               "title": article_base_channel.title_detail.value,
-                               "base_link": article_base_channel.title_detail.base,
-                               "image": article_base_channel.image.href,
-                               "language": article_base_channel.language,
-                               "rights_detail": article_base_channel.rights_detail.value,
-                               "generator": article_base_channel.generator,
-                               "updated": article_base_channel.updated,
-                               }
+def get_items(request):
+    items = Item.objects.all()
+    ordered = sorted(items, key=lambda item: datetime.strptime(item.published_date, '%Y-%m-%d %H:%M:%S'), reverse=True)
+    # ordered = sorted(items, key=operator.attrgetter('last_name'), reverse=True)
+    paginator = PageNumberPagination()
+    paginator.page_size = request.data['size']
+    result_page = paginator.paginate_queryset(ordered, request)
+    item_ser = ItemReadSerializer(result_page, many=True)
+    return paginator.get_paginated_response(item_ser.data)
 
-    # print(article_channel_context)
 
-    return JsonResponse(article_channel_context, status=200, safe=False)
+@api_view(['GET', ])
+def get_channel(request):
+    chan = Channel.objects.all()
+    paginator = PageNumberPagination()
+    result_page = paginator.paginate_queryset(chan, request)
+    chan_ser = ChannelReadSerializer(result_page, many=True)
+    return paginator.get_paginated_response(chan_ser.data)
