062ec6352a8242662a89fe7dbcc93d4321e223df
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-05 00:15:35 +0330

    "models of news items and channel added"

--- news/models.py
+++ news/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    title = models.TextField(unique=True)
+    summary = models.TextField()
+    link = models.TextField()
+    image_url = models.TextField()
+    published_date = models.TextField()
+
+
+class Channel(models.Model):
+    subtitle_detail = models.TextField()
+    title = models.TextField()
+    base_link = models.TextField(unique=True)
+    image = models.TextField()
+    language = models.TextField()
+    rights_detail = models.TextField()
+    generator = models.TextField()
+    updated = models.TextField()
