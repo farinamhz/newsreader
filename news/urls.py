717da80bf0d62d300e8893dbda1916f176789471
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-01-08 14:35:49 +0330

    "url for filtering changed"

--- news/urls.py
+++ news/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('read/fill/', views.fill_db, name='fill-db'),
     path('read/items/', views.get_items, name='get-items'),
     path('read/channel/', views.get_channel, name='get-channel'),
-    path('read/items/by-time/', views.get_items_by_time, name='get-items-by-time'),
+    path('read/items/by-filter/', views.get_items_by_filter, name='get-items--by-filter'),
 ]
